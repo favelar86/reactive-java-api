5d1534688563af94b39ff5f0811238e5247227d3
Fabiano Avelar <fabianoavelar86@gmail.com>
2023-09-08 22:20:19 -0300

    Projeto criado

---
+++ src/main/java/br/com/ada/reactivejavasw/ReactiveJavaSwApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.reactivejavasw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveJavaSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveJavaSwApplication.class, args);
+	}
+
+}
